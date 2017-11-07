86ac054bd48ccddfd861e70f74c97cd42c5135d6
tscholy <christina.tschol@students.fhv.at>
2017-11-07 15:14:57 +0100

    added Genre Enum

---
+++ src/main/java/at/fhv/itb/sem5/team6/libman/shared/enums/Genre.java
@@ -0,0 +1,21 @@
+package at.fhv.itb.sem5.team6.libman.shared.enums;
+
+/**
+ * Created by Christina on 07.11.2017.
+ */
+public enum Genre {
+    ACTION,
+    THRILLER,
+    HORROR,
+    ROMANTIC,
+    DRAMA,
+    FANTASY,
+    DOCUMENTATION,
+    MYSTERY,
+    ADVENTURE,
+    SCIFI,
+    WESTERN,
+    WAR,
+    COMEDY,
+    CRIME
+}
